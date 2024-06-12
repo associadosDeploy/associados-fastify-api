722cbbf016fb37ef4c0175bd0441ac4f180097ea
Vinicius Castro <viniciusc.d.c@hotmail.com>
2024-06-11 23:51:12 -0300

    build

--- src/env/index.ts
+++ src/env/index.ts
@@ -15,8 +15,8 @@ const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_URL: z.string(),
   JWT_SECRET: z.string(),
-  APP_URL: z.string(),
   PORT: z.number().default(3333),
+  APP_URL: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
