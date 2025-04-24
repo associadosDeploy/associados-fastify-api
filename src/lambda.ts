9ac5249e48947a016377cf865daa5101e2516b46
associadosDeploy <89207339+associadosDeploy@users.noreply.github.com>
2025-04-24 03:41:43 -0300

    Delete src/lambda.ts

--- src/lambda.ts
+++
@@ -1,8 +0,0 @@
-const awsLambdaFastify = require('@fastify/aws-lambda')
-const init = require('./app');
-
-const proxy = awsLambdaFastify(init())
-// or
-// const proxy = awsLambdaFastify(init(), { binaryMimeTypes: ['application/octet-stream'] })
-
-exports.handler = proxy;
