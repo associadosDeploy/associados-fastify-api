86ef96937e334c58ff2356c1f430aa3279533686
associadosDeploy <89207339+associadosDeploy@users.noreply.github.com>
2025-04-28 11:46:11 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,10 @@
 import { app } from './http/app'
 import { env } from './env'
 
-function init() {
-  return app;
-}
-
-if (require.main === module) {
-  init().listen({ port: 3333 }).then(() => {
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
     console.log('HTTP server listening')
   })
-} else {
-  // required as a module => executed on aws lambda
-  module.exports = init;
-}
